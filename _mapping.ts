8f4ab445067d0246d88eea22bde02919d86bc574
Matthew5 <jnwu@thoughtworks.com>
2019-07-11 22:11:07 +0800

    Add contract

---
+++ _mapping.ts
@@ -0,0 +1,22 @@
+export default {
+  vue: {
+    addVue: {
+      req: 'AddVueReq',
+      res: '',
+    },
+    getVue: {
+      req: 'GetVueReq',
+      res: 'GetVueRes',
+    },
+    updateVue: {
+      req: 'UpdateVueReq',
+      res: '',
+    },
+  },
+  qiniu: {
+    getQiniuToken: {
+      req: '',
+      res: 'GetQiniuTokenRes',
+    },
+  },
+}
