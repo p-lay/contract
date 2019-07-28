e43ffe8464c591e0147512468d06d651cc401bcd
Matthew5 <jnwu@thoughtworks.com>
2019-07-28 23:17:49 +0800

    fix: tag sys

--- global.ts
+++ global.ts
@@ -10,5 +10,6 @@ export type CommonRes<T = any> = Promise<{
  * 1000: empty value not exception;
  * 2000: no data in table exception;
  * 3000: data has relation, cannot be delete
+ * 3100: duplicate data in table
  */
-export type Code = 500 | 1000 | 2000 | 3000
+export type Code = 500 | 1000 | 2000 | 3000 | 3100
